2d1ac4ed294ad09f67b92fd46545000ea8b337c1
navnitchaudhary-condenast <navnit_chaudhary@condenast.com>
2022-02-08 15:30:51 +0530

    added dashboard page

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -1,108 +1,39 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
-import CameraIcon from "@mui/icons-material/PhotoCamera";
-import CssBaseline from "@mui/material/CssBaseline";
-import { DataGrid } from "@mui/x-data-grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
+      {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 }
 
-const columns = [
-  {
-    field: "profilePicture",
-    headerName: "Picture",
-    width: 80,
-    renderCell: ({ value, row: { firstName, lastName } }) => (
-      <img src={value} alt={`${firstName} ${lastName}`} />
-    ),
-  },
-  { field: "email", headerName: "Email", width: 250 },
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
-  },
-  {
-    field: "engagementScore",
-    headerName: "Engagement Score",
-    type: "number",
-    width: 110,
-  },
-  {
-    field: "isCurrentUser",
-    renderCell: ({ value, row: { id } }) =>
-      value ? (
-        <button onClick={() => alert(`Edit user with ID: ${id}`)}>Edit</button>
-      ) : (
-        <></>
-      ),
-  },
-];
-const rows = [
-  {
-    id: 1,
-    profilePicture: "https://source.unsplash.com/random",
-    email: "joe.bourne@condenast.com",
-    firstName: "Joe",
-    lastName: "Bourne",
-    engagementScore: 14,
-    isCurrentUser: true,
-  },
-  {
-    id: 2,
-    profilePicture: "https://source.unsplash.com/random",
-    email: "sue.perkins@condenast.com",
-    firstName: "Sue",
-    lastName: "Perkins",
-    engagementScore: 95,
-    isCurrentUser: false,
-  },
-  {
-    id: 3,
-    profilePicture: "https://source.unsplash.com/random",
-    email: "greg.wallace@condenast.com",
-    firstName: "Greg",
-    lastName: "Wallace",
-    engagementScore: 45,
-    isCurrentUser: false,
-  },
-  {
-    id: 4,
-    profilePicture: "https://source.unsplash.com/random",
-    email: "matt.lucas@condenast.com",
-    firstName: "Matt",
-    lastName: "Lucas",
-    engagementScore: 87,
-    isCurrentUser: false,
-  },
-];
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
-const DashBoard = () => {
+export default function Album() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -118,7 +49,7 @@ const DashBoard = () => {
         {/* Hero unit */}
         <Box
           sx={{
-            bgcolor: "background.paper",
+            bgcolor: 'background.paper',
             pt: 8,
             pb: 6,
           }}
@@ -133,15 +64,10 @@ const DashBoard = () => {
             >
               Album layout
             </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Something short and leading about the collection below—its contents,
+              the creator, etc. Make it short and sweet, but not too short so folks
+              don&apos;t simply skip over it entirely.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -155,17 +81,43 @@ const DashBoard = () => {
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
-          <DataGrid
-            autoHeight
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-          />
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </Container>
       </main>
       {/* Footer */}
-      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
         <Typography variant="h6" align="center" gutterBottom>
           Footer
         </Typography>
@@ -182,6 +134,4 @@ const DashBoard = () => {
       {/* End footer */}
     </ThemeProvider>
   );
-};
-
-export default DashBoard;
+}
