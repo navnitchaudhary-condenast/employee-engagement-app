7a1d0bfa1332121ddfd50ad9cd5ebb4819bc8054
Karthik RP <blendedidea@gmail.com>
2022-02-09 19:37:00 +0530

    Add fact dialog.
    Signed-off-by: Karthik RP <blendedidea@gmail.com>

--- src/components/analytics/subcomponents/FactsManagement.js
+++ src/components/analytics/subcomponents/FactsManagement.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Typography, Dialog, DialogContent, DialogActions, DialogTitle, Button, TextField, Box } from '@mui/material';
 import { DataGrid } from "@mui/x-data-grid";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faTimesCircle, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 const FactsManagement = () => {
     const columns = [
@@ -24,34 +24,71 @@ const FactsManagement = () => {
         },
     ];
 
-    const rows = [
+    const [rows, setRows] = useState([
         { id: 1, fact: 'Approaches to covers have changed dramatically over the years at Vogue, from Edward Steichen’s first photographic color cover in 1932, to Salvador Dali\'s signature surrealist, dystopian landscape. The title delights in offering an element of surprise.', published: true },
         { id: 2, fact: 'The first person convicted of speeding was going eight mph.', published: false },
         { id: 3, fact: 'India has one of the largest postal networks on the planet; including a floating post office.', published: true },
         { id: 4, fact: 'The world wastes about 1 billion metric tons of food each year.', published: false },
-    ]
+    ]);
+    const [displayAddFactDialog, setDisplayAddFactDialog] = useState(false);
+    const [newFact, setNewFact] = useState('');
+
+    function handleAddFact() {
+        setRows([...rows, { id: rows.length + 1, fact: newFact, published: false }]);
+        setNewFact('');
+        setDisplayAddFactDialog(false);
+    }
 
     return (
-        <Container sx={{ mt: 8 }} maxWidth="md">
-            <Typography
-                component="h2"
-                variant="h5"
-                align="center"
-                color="text.primary"
-                mb={3}
-            >
-                Manage Facts
-            </Typography>
+        <>
+            <Container sx={{ mt: 8, mb: 3 }} maxWidth="md">
+                <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px'}}>
+                    <Typography
+                        component="h2"
+                        variant="h5"
+                        align="center"
+                        color="text.primary"
+                    >
+                        Manage Facts
+                    </Typography>
 
+                    <FontAwesomeIcon icon={faPlusCircle} style={{cursor: 'pointer'}} onClick={() => setDisplayAddFactDialog(true)}/>
+                </Box>
 
-            <DataGrid
-                autoHeight
-                rows={rows}
-                columns={columns}
-                pageSize={10}
-                rowsPerPageOptions={[10]}
-            />
-        </Container>
+                <DataGrid
+                    autoHeight
+                    rows={rows}
+                    columns={columns}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                />
+            </Container>
+
+            <Dialog onClose={() => {
+                setDisplayAddFactDialog(false);
+            }} open={displayAddFactDialog}
+                fullWidth
+                maxWidth="sm">
+                <DialogTitle>Add Fact</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="fact"
+                        label="Fact"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={newFact}
+                        onChange={(e) => setNewFact(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setDisplayAddFactDialog(false)} color='error'>Cancel</Button>
+                    <Button onClick={handleAddFact} variant='contained'>Add</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
 
