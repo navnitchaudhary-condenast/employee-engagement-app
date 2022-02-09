604a8ccb523a483caca6d85966fb1cdd05b5fe8c
Joe Bourne <josephbourne14@gmail.com>
2022-02-09 12:56:50 +0000

    prettify footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,12 +4,7 @@ import Typography from "@mui/material/Typography";
 
 const Copyright = (props) => {
   return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
+    <Typography variant="body2" align="center" {...props}>
       Copyright © Jiffy {new Date().getFullYear()}.
     </Typography>
   );
@@ -17,16 +12,20 @@ const Copyright = (props) => {
 
 const Footer = () => {
   return (
-    <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+    <Box
+      sx={{ bgcolor: "darkblue", p: 6 }}
+      component="footer"
+      fullWidth
+      color="white"
+      style={{
+        backgroundColor: "#2a2a72",
+        backgroundImage: "linear-gradient(315deg, #2a2a72 0%, #1976d2 74%)",
+      }}
+    >
       <Typography variant="h6" align="center" gutterBottom>
         Got an idea for an engagement activity?
       </Typography>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        color="text.secondary"
-        component="p"
-      >
+      <Typography variant="subtitle1" align="center" component="p">
         Don't hesitate to{" "}
         <Link color="inherit" href="mailto:support@jiffy.com">
           email us
