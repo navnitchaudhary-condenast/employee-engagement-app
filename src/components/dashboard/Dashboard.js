bfb1d555ad364f652f4610909e91e08419492b34
Joe Bourne <josephbourne14@gmail.com>
2022-02-09 11:08:40 +0000

    feat: conditionally render analytics dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+
+import { userSelector } from "../../slices/user";
+
 import EngagementScore from "../engagementScore/EngagementScore";
+import Analytics from "../analytics/Analytics";
 
 const Dashboard = () => {
-  return <EngagementScore />;
+  const { user } = useSelector(userSelector);
+
+  return user.role === "user" ? <EngagementScore /> : <Analytics />;
 };
 
 export default Dashboard;
